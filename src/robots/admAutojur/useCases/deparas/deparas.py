6370a95940451d48d2e4cf9a76489bb8931e9575
Yann Lima <yann.lima@docato.com.br>
2024-02-21 21:48:27 -0300

    Add novo tipo_sistema no depara

--- src/robots/admAutojur/useCases/deparas/deparas.py
+++ src/robots/admAutojur/useCases/deparas/deparas.py
@@ -3,6 +3,7 @@ class Deparas:
     def depara_autoridade(tipo_sistema):
         depara = {
             "PROCON":"27286",
-            "PROCON / Consumidor.gov":"27287"
+            "PROCON / Consumidor.gov":"27287",
+            "PROCON / Consumidor.gov.br":"27287"
         }
         return depara.get(tipo_sistema) 
