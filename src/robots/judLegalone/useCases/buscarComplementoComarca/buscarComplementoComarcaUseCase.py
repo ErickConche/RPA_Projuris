ba048432434fb65d82d96d4640b44cef0169f4d1
JOAO VICTOR DE LIMA MARTINS <joao.martins@docato.com.br>
2024-03-04 13:44:40 +0000

    APP-1053
    commit 8721757d 
    Author: Yann Lima <yann.lima@docato.com.br> 
    Date: Mon Mar 04 2024 10:37:31 GMT-0300 (Horário Padrão de Brasília) 
    
        Retirando comentarios
    
    
    commit 9c1ac6db 
    Author: Yann Lima <yann.lima@docato.com.br> 
    Date: Mon Mar 04 2024 10:33:48 GMT-0300 (Horário Padrão de Brasília) 
    
        Inserção de complemento de comarca ou complemento de vara quando esses não existirem no legalone

--- src/robots/judLegalone/useCases/buscarComplementoComarca/buscarComplementoComarcaUseCase.py
+++ src/robots/judLegalone/useCases/buscarComplementoComarca/buscarComplementoComarcaUseCase.py
@@ -1,10 +1,14 @@
 import json
 import time
+from urllib.parse import urlencode
 from playwright.sync_api import Page, BrowserContext, sync_playwright
 import requests
 from unidecode import unidecode
 
 from modules.logger.Logger import Logger
+from robots.admAutojur.useCases.deparas.deparas import Deparas
+from robots.admLegalone.useCases.buscarDadosUf.buscarDadosUfUseCase import BuscarDadosUfUseCase
+from robots.admLegalone.useCases.buscarDadosUfCidade.buscarDadosUfCidadeUseCase import BuscarDadosUfCidadeUseCase
 
 class BuscarComplementoComarcaUseCase:
     def __init__(
@@ -12,9 +16,15 @@ class BuscarComplementoComarcaUseCase:
         complemento_comarca: str,
         classLogger: Logger,
         id_comarca: int,
+        id_uf: str,
+        uf:str,
+        nome_comarca: str,
         context: BrowserContext
     ) -> None:
+        self.id_uf = id_uf
+        self.uf = uf
         self.id_comarca = id_comarca
+        self.nome_comarca = nome_comarca
         self.complemento_comarca = complemento_comarca
         self.classLogger = classLogger
         self.context = context
@@ -66,8 +76,36 @@ class BuscarComplementoComarcaUseCase:
                 time.sleep(0.5)
                 cont +=1
 
+            url = f"https://booking.nextlegalone.com.br/config/ComplementoForo/EditModal"
+
+            uf_name = BuscarDadosUfCidadeUseCase(uf=self.uf,classLogger=self.classLogger,context=self.context).execute()
+
+            body = urlencode({
+                "JusticaText":"Justiça Estadual",
+                "JusticaId":"13",
+                "UFText":uf_name.get("UFText"),
+                "UFId":uf_name.get("Id"),
+                "ForoText":self.nome_comarca,
+                "ForoId":self.id_comarca,
+                "Descricao":self.complemento_comarca
+            })
+
+            headers = {
+                "X-Requested-With":"XMLHttpRequest",
+                "Referer":url,
+                "Host":"booking.nextlegalone.com.br",
+                "Cookie":cookies_str,
+                "Content-Type":"application/x-www-form-urlencoded"
+            }
+
+            response = requests.post(url=url,data=body,headers=headers)
+
+            if response.status_code == 200:
+                return json_response
+
             raise Exception ("O complemento da comarca não foi encontrado")
         except Exception as error:
             message = "Erro ao buscar o cadastro do complemento da comarca no Legalone"
             self.classLogger.message(message)
             raise error
+        
