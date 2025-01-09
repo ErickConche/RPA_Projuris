0336fcfbec0c5c603edfd17b3ca6ed4f3850bdd6
ErickConche <60793312+ErickConche@users.noreply.github.com>
2025-01-09 01:01:10 -0300

    Delete src/robots/autojur directory

--- src/robots/autojur/admAutojur/useCases/deparas/deparas.py
+++
@@ -1,9 +0,0 @@
-
-class Deparas:
-    def depara_autoridade(tipo_sistema):
-        depara = {
-            "PROCON":"27286",
-            "PROCON / Consumidor.gov":"27287",
-            "PROCON / Consumidor.gov.br":"27287"
-        }
-        return depara.get(tipo_sistema) 
