9ef133ef36956e18aacf8b754b09916ee0d201f0
ErickConche <60793312+ErickConche@users.noreply.github.com>
2025-01-09 01:05:31 -0300

    Delete src/modules/excecoes directory

--- src/modules/excecoes/excecao.py
+++
@@ -1,6 +0,0 @@
-
-class ExcecaoGeral(Exception):
-    def __init__(self, log_erro, msg_erro: str = "SITE INDISPONÍVEL"):
-        super().__init__(msg_erro)
-        self.log_erro = log_erro
-        self.msg_erro = msg_erro
