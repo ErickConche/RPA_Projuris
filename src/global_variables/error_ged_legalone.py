d65d2fd397b62a37cbe8f737bb26de510d88fe44
ErickConche <60793312+ErickConche@users.noreply.github.com>
2025-01-09 01:04:25 -0300

    Delete src/global_variables directory

--- src/global_variables/error_ged_legalone.py
+++
@@ -1,9 +0,0 @@
-error_ged_legalone = False
-
-def get_error_ged_legalone():
-    global error_ged_legalone
-    return error_ged_legalone
-
-def update_error_ged_legalone(status: bool):
-    global error_ged_legalone
-    error_ged_legalone = status
