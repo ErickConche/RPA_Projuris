30bb18f2e59659792c84e2e6427fad809c7a47a9
ErickConche <60793312+ErickConche@users.noreply.github.com>
2025-01-09 01:05:02 -0300

    Delete src/modules/validacao directory

--- src/modules/validacao/validacao.py
+++
@@ -1,9 +0,0 @@
-from modules.validacao.useCases.validarHoraUseCase import ValidarHoraUseCase
-
-
-class Validacao:
-    def __init__(self) -> None:
-        pass
-
-    def validar_hora(self, hora: str):
-        return ValidarHoraUseCase(hora=hora).execute()
